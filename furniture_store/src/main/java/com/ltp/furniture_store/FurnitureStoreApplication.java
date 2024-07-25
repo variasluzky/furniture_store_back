407f2ce904500fc7ebaa0444b6be1de7c8533ca9
variasluzky <77620351+variasluzky@users.noreply.github.com>
2024-07-25 21:29:28 +0300

    Add files via upload

---
+++ furniture_store/src/main/java/com/ltp/furniture_store/FurnitureStoreApplication.java
@@ -0,0 +1,13 @@
+package com.ltp.furniture_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnitureStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurnitureStoreApplication.class, args);
+	}
+
+}
---
+++ furniture_store/src/test/java/com/ltp/furniture_store/FurnitureStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltp.furniture_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnitureStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
